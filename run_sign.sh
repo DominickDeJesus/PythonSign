23e90214cf7f3fc181b841836c25c6e0189d0e6f
Dominick DeJesus <dominick.dejesus@gmail.com>
2025-03-28 11:24:18 -0400

    new images

--- run_sign.sh
+++ run_sign.sh
@@ -6,5 +6,7 @@ sleep 2
 # Start LED controller in tmux
 tmux new-session -d -s controller 'python3 /home/pi/sign/led_display.py'
 
+sleep 2
+
 # Start Flask server in tmux
 tmux new-session -d -s server 'python3 /home/pi/sign/server.py'
