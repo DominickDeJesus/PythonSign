a687cf35daa4b5815de5246a49d31cf7e90dced8
DominickDeJesus <dominick.dejesus@gmail.com>
2025-03-28 00:23:30 -0400

    updated scripts

--- run_sign.sh
+++ run_sign.sh
@@ -3,5 +3,5 @@
 #tmux new-session -d -s sign
 #tmux send-key 'sudo python /home/pi/sign/led_display.py' C-m
 tmux new -d -s sign-controller 'python /home/pi/sign/led_display.py'
-tmux new -d -s sign-server 'python /home/pi/sign/led_display.py'
+tmux new -d -s sign-server 'python /home/pi/sign/server.py'
 #cd ~
